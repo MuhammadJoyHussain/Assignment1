b4004966185aba9402d7251d29fd06969a7ebf26
unknown <muhammadjoyhussain@gmail.comdtrujhtyjnjjtdjhnghj>
2021-01-22 22:12:48 +0600

    Assignment.js

--- assignment.js
+++ assignment.js
@@ -1,81 +1,93 @@
 function kilometerToMeter(kilometer){
     var meter;
-    if(kilometer<0)
+    if(kilometer < 0)
     {
         meter = "Kilometer can't be Negetive";
     }
+    //1 kilometer = 1000 meter
     else
     {
         meter = kilometer * 1000;
     }
     return meter;
 }
-var result = kilometerToMeter(16);
-console.log(result);
 
 
 
-function budgetCalculator(noOfwatch, noOfPhone, noOfLaptop)
+
+//2
+function budgetCalculator(numOfwatch, numOfPhone, numOfLaptop)
  {
-     var cost;
-     if(noOfwatch<0 || noOfPhone<0 || noOfLaptop<0)
+     if(numOfwatch < 0 || numOfPhone < 0 || numOfLaptop < 0)
      {
          cost = "Amount can't be less then zero."
      }
-     else if(noOfwatch%1!=0 || noOfPhone%1!=0 || noOfLaptop%1!=0)
+     // Modulus of a number by 1 because if answer is 0 then it indicate the number is integer.
+     else if(numOfwatch %1!= 0 || numOfPhone %1!= 0 || numOfLaptop %1!= 0)
      {
          cost = "Amount should be integer number";
      }
      else
-        cost = 50*noOfwatch + 100*noOfPhone + 500*noOfLaptop;
+        cost = 50 * numOfwatch + 100 * numOfPhone + 500 * numOfLaptop;
      return cost;
  }
- var cost = budgetCalculator(5, 10, 15);
- console.log(cost);
 
 
 
 
+//3
  function hotelCost(stay)
  {
      var cost = 0;
-     if(stay<0)
+     if(stay < 0)
      {
          cost = "Noumber of day can't be negetive";
      }
+     // If the given input is valid then calculated the result.
      else
      {
-        if(stay>20)
+          /*
+            1. Firstly, calculate the cost for greater then 20 days (if the day is >20).
+            2. Secondly, calculate the cost for greater then 10 days (if the day is >10).
+            3.Finaly, calculate the cost for less then or equal to 10 days (if the day is <=10).
+         */
+        if(stay > 20)
         {
-            cost = (stay-20)*50;
+            cost = (stay-20) * 50;
             stay = 20;
         }
-        if(stay>10)
+        if(stay > 10)
         {
-            cost = cost + (stay-10)*80;
+            cost = cost + (stay-10) * 80;
             stay = 10;
         }
-        cost = cost + stay*100;
+        cost = cost + stay * 100;
      }
      return cost;
  }
- var hotelCost = hotelCost(21);
- console.log(hotelCost);
 
 
 
+
+
+//4
+ var Name = ["Joynul", "Jafrul", "Rasel", "Salah", "Ashraful", "Kamruzzaman", "Ali"];
  function megaFriend(friendsName)
- {
+ {   
     var maxName = "";
     var max = 0;
+    
     if(friendsName.length==0)
-     return "There is no friends name";
-    for(var i=0; i<friendsName.length; i++)
-    {
+  
+    return "There is no friends name";
+    
+     for(var i = 0; i < friendsName.length; i++){
        n = friendsName[i].length;
-       for(var j=0; j<n; j++)
+       //nested loop used for access character by character of a string.
+       for(var j = 0; j < n; j++)
        {
            //Check whether the given name is valid or not!
+           //valid name is always an alphabet(a-z,A-Z) following by space between two separator so check this sequence.
            if( !( (friendsName[i][j]>='a' && friendsName[i][j]<='z')  || (friendsName[i][j]>='A' && friendsName[i][j]<='Z') || friendsName[i][j]==' ') )
            {
                return "Name should contain alphabet";
@@ -90,6 +102,4 @@ function budgetCalculator(noOfwatch, noOfPhone, noOfLaptop)
     }
     return maxName;
  }
- var Name = ["Joynul", "Jafrul", "Rasel", "Salah", "Ashraful", "Kamruzzaman", "Ali"];
- var Fname = megaFriend(Name);
- console.log(Fname);
+ 
